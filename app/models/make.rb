c57c7bfbcc9af098decb451cf60237730acc96cf
trevor <enabfluw@gmail.com>
2013-03-11 20:07:07 -0700

    validations

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,4 +1,10 @@
 class Make < ActiveRecord::Base
   attr_accessible :name, :website
   has_many :cars
+
+  with_options :presence => true do |make|
+    make.validates :name
+    make.validates :website
+  end
+
 end
